91db24ddd8e9e430009a126e1da400b89d8888f0
KimSujin02 <s2018w28@e-mirim.hs.kr>
2019-10-21 09:49:56 +0900

    아마스빈 내역 저장하고 불러오고 이전에 주문한 총 금액 구하기

--- 2학기/amasbin/main.py
+++ 2학기/amasbin/main.py
@@ -1,5 +1,23 @@
-#main.py
 from order import Order
+from file_Manager import FileManager
+
+file_Manager = FileManager("history.bin")
+
+#history 있으면 내역 보여주자
+history = []
+try:
+    history = file_Manager.load() #[Drink 객체, Drink 객체]
+    sum = 0
+    for h in history:
+        print(h)
+        sum += h.price
+    print("여태껏 아마스빈에 퍼부운 내 돈 : " + str(sum) + "원")
+except FileNotFoundError:
+    print("내역이 없습니다.")
+print("--------------------------------------")
+
 o = Order()
 o.order_drink()
 
+#history에 저장하자
+file_Manager.save(o.order_menu)
